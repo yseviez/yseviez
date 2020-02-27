5e2ea9afd708aafb6af94462219e77cf67aacd8b
Pedro Plasencia <pjpv9011@gmail.com>
2020-02-27 20:43:21 +0100

    web con validaciones

---
+++ assets/js/email-form.js
@@ -0,0 +1,49 @@
+$(function () {
+	function isNullOrWhitespace(input) {
+
+		if (typeof input === 'undefined' || input == null) return true;
+
+		return input.replace(/\s/g, '').length < 1;
+	}
+	jQuery.validator.addMethod("validateNullOrWhiteSpace", function (value, element) {
+		return !isNullOrWhitespace(value);
+	}, "No se permiten valores en blanco");
+	// Initialize form validation on the registration form.
+	// It has the name attribute "registration"
+	$("form[name='email']").validate({
+		// Specify validation rules
+		rules: {
+			// The key name on the left side is the name attribute
+			// of an input field. Validation rules are defined
+			// on the right side
+			name: {
+				required: true,
+				validateNullOrWhiteSpace: true
+			},
+			email: {
+				required: true,
+				// Specify that email should be validated
+				// by the built-in "email" rule
+				email: true,
+				validateNullOrWhiteSpace: true
+			},
+			message: {
+				required: true
+			}
+		},
+		// Specify validation error messages
+		messages: {
+			name: "Por favor introduce tu nombre",
+			message: {
+				required: "Por favor introduce tu mensaje"
+			},
+			email: "Por favor introduce un email válido"
+		},
+		// Make sure the form is submitted to the destination defined
+		// in the "action" attribute of the form when valid
+		submitHandler: function (form) {
+			form.submit();
+		}
+	});
+});
+
